6b1016c5e7b8e3a2a98b715085955dffdd6007b9
SwiftDust <mart.zielman@kpnmail.nl>
2025-07-17 23:17:08 +0200

    Make sure astro-i18n config is included

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
+import "./astro-i18n.config.mjs";
 
 export default defineConfig({
   adapter: vercel(),
