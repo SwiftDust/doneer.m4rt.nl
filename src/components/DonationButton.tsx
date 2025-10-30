fbfb8c03848d7d98957ff61ecb9d4df13621b138
SwiftDust <mart.zielman@kpnmail.nl>
2025-10-30 11:32:51 +0100

    Remove headers that shouldn't be there

--- src/components/DonationButton.tsx
+++ src/components/DonationButton.tsx
@@ -29,8 +29,6 @@ interface Subscriber {
 function submitSubscriber(subscriber: Subscriber): Promise<Subscriber[]> {
   const headers: Headers = new Headers();
   headers.set("Content-Type", "application/json");
-  headers.set("Access-Control-Allow-Origin", "*");
-  headers.set("Accept", "application/json");
   headers.set("Authorization", `token api:${import.meta.env.TOKEN}`);
 
   const request = new Request("https://listmonk.m4rt.nl/api/subscribers", {
